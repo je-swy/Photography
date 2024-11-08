472c1094e5ad5e905e16eace05b1f9d8fe125288
je-swy <kateryna.shemonaieva@gmail.com>
2024-11-08 11:37:29 +0200

    fixed inst gallery

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ const swiper = new Swiper(".swiper", {
 const instagram = document.querySelector(".instagram__flex")
 
 Array.from(instagram.children).forEach((item) => {
-    const duplicateNode = item.cloneNode(true);
-    duplicateNode.setAttribute("aria-hidden", true);
-    instagram.appendChild(duplicateNode);
-})
+  const duplicateNode = item.cloneNode(true);
+  duplicateNode.setAttribute("aria-hidden", true);
+  instagram.appendChild(duplicateNode);
+});
