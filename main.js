8f38ba09137276666cf463c092e9c7f15fdf2b72
je-swy <kateryna.shemonaieva@gmail.com>
2024-11-08 11:28:01 +0200

    add inst gallery and footer

--- main.js
+++ main.js
@@ -62,3 +62,11 @@ const swiper = new Swiper(".swiper", {
         el: ".swiper-pagination",
     },
 });
+
+const instagram = document.querySelector(".instagram__flex")
+
+Array.from(instagram.children).forEach((item) => {
+    const duplicateNode = item.cloneNode(true);
+    duplicateNode.setAttribute("aria-hidden", true);
+    instagram.appendChild(duplicateNode);
+})
